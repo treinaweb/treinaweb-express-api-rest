30fb3a6bf1b22f14c99e5c5dae6ee3214fedb98a
Wesley Gado <wesleygado@gmail.com>
2025-11-06 16:53:21 -0300

    utilizando middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,20 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 const router = Router();
+
 const DATABASE_BOOKS = [
   { id: 1, nome: "Hábitos Atomicos", ano: 2024 },
   { id: 2, nome: "Game of Thrones", ano: 2020 },
   { id: 3, nome: "Senhor dos anéis", ano: 1980 },
   { id: 4, nome: "Neuromancer", ano: 1980 },
 ];
+
+const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+}
+
+router.use(loggerMiddleware);
 
 router.post("/books", (req: Request, res: Response) => {
   const book = req.body;
